bd3d0fda1dd0859fbdd6f01a47e0d96221ce8ecd
the100-00 <alak10@naver.com>
2020-02-19 15:50:18 +0900

    4_1

---
+++ OneDrive/바탕 화면/JAVA 복습/4_1/src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+	String name;
+	int rollno;
+	int age;
+	Student(String name, int rollno, int age) {
+		this.name = name;
+		this.rollno = rollno;
+		this.age = age;
+	}
+}
---
+++ OneDrive/바탕 화면/JAVA 복습/4_1/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj = new Student("Kim",20180001,20);
+		
+		System.out.print("학생의 이름: "+obj.name+"\n학생의 학번: "+obj.rollno+"\n학생의 나이: "+obj.age);
+	}
+}
